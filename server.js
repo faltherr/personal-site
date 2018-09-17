90ed6f4fbfc6421a8490af8d74800d738bd2f06b
L245854 <l245854@pn1706924.lanl.gov>
2018-09-17 12:29:39 -0600

    Updated portfolio package for build

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const bodyParser = require ('body-parser')
 
 const app = express()
 
-app.use('/static', express.static('portfolio'))
+app.use(express.static(__dirname + '/portfolio'))
 
 app.use(bodyParser.json())
 app.listen(7003, ()=> {
-console.log('Listening on port 4000')
+console.log('Listening on port 7003')
 })
